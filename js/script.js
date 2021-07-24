45664c524ea4ee3e1bf416753567c9152aa13c8a
Kamruj akash <source.akash@gmail.com>
2021-07-24 10:09:44 +0600

    responsive and img sizeing

--- js/script.js
+++ js/script.js
@@ -35,19 +35,17 @@ $('.servicer-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true,
-            }
-        },
-        {
-            breakpoint: 600,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
             }
         },
         {
